315b5c8934d682df6fbec0df7121b3fb7ccfb5f9
Halil Yüksel <haloyuk.02@gmail.com>
2024-03-17 14:45:30 +0200

    Register From Updated

--- frontend/src/Component/RegisterForm/RegisterForm.jsx
+++ frontend/src/Component/RegisterForm/RegisterForm.jsx
@@ -1,41 +1,102 @@
-import React from "react"
+import React from "react";
 import { LuUser2 } from "react-icons/lu";
-import "./RegisterForm.css"
-
+import "./RegisterForm.css";
+import { useState } from "react";
 
 function RegisterForm() {
+    const [inputs, setInputs] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+        confirmedPassword: "",
+        gender: "",
+    });
+
+    const handleCheckBoxChange = (gender) => {
+        setInputs({ ...inputs, gender });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputs);
+    };
+
     return (
         <div className="wrapper">
-        <form action="">
-            <div className="logo"></div>
-            <div className="input-box">
-                <LuUser2 /><input type="text" placeholder="Name" required/>
-            </div>
-            <div className="input-box">
-                <input type="text" placeholder="Surname" required/>
-            </div>
-            <div className="input-box">
-            <input type="text" placeholder='Username' required/> 
-            </div>
-            <div className="input-box">
-             <input type="password" placeholder='Password' required/> 
-            </div>
-            <div className="input-box">
-             <input type="password" placeholder='Canfirmed Password' required/> 
-            </div>
-            <div className="input-box">
-                <input type="email" placeholder="E-mail" required/>
-            </div>
-            <p>Select your gender:         </p>  
-            <input type="radio" name="gender" value="male" required/>
-            <label for="male">Male   </label>
-            <input type="radio" name="gender" value="female" required/>
-            <label for="female">   Female</label><br/><br/>
-            
-                <button type='submit'>Register</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <div className="logo"></div>
+                <div className="input-box">
+                    <LuUser2 />
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        required
+                        value={inputs.fullName}
+                        onChange={(e) =>
+                            setInputs({
+                                ...inputs,
+                                fullName: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div className="input-box">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        required
+                        value={inputs.username}
+                        onChange={(e) =>
+                            setInputs({
+                                ...inputs,
+                                username: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div className="input-box">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={inputs.password}
+                        onChange={(e) =>
+                            setInputs({
+                                ...inputs,
+                                password: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div className="input-box">
+                    <input
+                        type="password"
+                        placeholder="Canfirmed Password"
+                        required
+                        value={inputs.confirmedPassword}
+                        onChange={(e) =>
+                            setInputs({
+                                ...inputs,
+                                confirmedPassword: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div className="input-box">
+                    <input type="email" placeholder="E-mail" required />
+                </div>
+                <p>Select your gender: </p>
+                <input type="radio" name="gender" value="male" required />
+                <label for="male">Male </label>
+                <input type="radio" name="gender" value="female" required />
+                <label for="female"> Female</label>
+                <br />
+                <br />
+
+                <button type="submit">Register</button>
+            </form>
         </div>
     );
 }
 
-export default RegisterForm
+export default RegisterForm;
